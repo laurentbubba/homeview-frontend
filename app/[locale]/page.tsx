5891cf0f90c354ea04d92def8e953e7a6a2c78c4
UCLL <laurent.bubba@hotmail.com>
2025-09-17 15:13:09 +0200

    added LocaleSwitcher

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import { useTranslations } from "next-intl";
-import Image from "next/image";
+import { LocaleSwitcher } from "./_components/LocaleSwitcher";
 
 export default function Home() {
   const t = useTranslations();
@@ -8,6 +10,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <p>{t('genta.love')}</p>
+        <LocaleSwitcher/>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         {/* extra links and socials etc? */}
